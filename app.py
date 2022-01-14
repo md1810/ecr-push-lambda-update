6ce4c989ae4f82e8d7344882bfec86eb8a90d720
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-15 02:22:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-import json
-
+import boto3
+import pandas as pd
+import pickle
+import lightgbm
+BUCKET = 'bucket_name'
+FILE_TO_READ = 'pickle_file.pkl'
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    s3 = boto3.client('s3')
+    data = s3.get_object(Bucket=BUCKET, Key=FILE_TO_READ)
+    model = pickle.loads(data['Body'].read())
+    data_for_prediction = pd.Series(event).to_frame().T
+    res = {'probability': float(model.predict_proba(data_for_prediction)[0][1]),
+           'prediction': float(model.predict(data_for_prediction)[0])}
+    return res
